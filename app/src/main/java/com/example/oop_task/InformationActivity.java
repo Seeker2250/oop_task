6e8b476635c53a0d766a909d644b698fc4ccdb50
Seeker2250 <wnsdnals@naver.com>
2023-04-13 10:47:28 +0900

    commit3 intent 및 모든 Activity 완전 구현

--- app/src/main/java/com/example/oop_task/InformationActivity.java
+++ app/src/main/java/com/example/oop_task/InformationActivity.java
@@ -1,6 +1,69 @@
 package com.example.oop_task;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InformationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+
+        Intent it = getIntent();//저쪽에서 보내온 정보 파악만 하고 사용은 안하지만 그래도 Intent 받아오자
+        String str = it.getStringExtra("id");// str에 id 담아놔
+
+        //참조변수 이름과 Controller 이름 동일하게
+        EditText editTextName = findViewById(R.id.editTextName);//헷갈리지 않게 조심
+        EditText editTextAge = findViewById(R.id.editTextAge);//헷갈리지 않게 조심
+        RadioButton radioMale = findViewById((R.id.radioMale));
+        RadioButton radioFemale = findViewById((R.id.radioFemale));
+        CheckBox cBInfo = findViewById(R.id.cBInfo);
+        CheckBox cBAI = findViewById(R.id.cBAI);
+        CheckBox cBSecurity = findViewById(R.id.cBSecurity);
+        Button btnSend = findViewById(R.id.btnSend);
+        //다 인식시켜둔거야
+
+        //btnSend 차례
+        //전송하기 버튼을 누르면 Information에 있는 정보를 capture해서 Main으로 보내주고 종료해야해
+        //해야할 일 구현하려면 Listener 달아줘야지
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent();//매개변수 필요없어 실행요청이 아니라 그냥 정보 보내는거니까
+                it.putExtra("name",editTextName.getText().toString());//인텐트 객체에 정보를 담아야지
+                //name이라는 key로 editTextName에 담겨져 있던 text가져와서 String으로
+                it.putExtra("age", editTextAge.getText().toString());
+                //나이도 그냥 String취급해 출력하기 위한거니까
+
+                if(radioMale.isChecked())
+                    it.putExtra("sex", "male");
+                else
+                    it.putExtra("sex", "female");
+                //남성버튼에 체크면 남성 아니면 여성
+
+                String strLicense = "";
+                if (cBInfo.isChecked())
+                    strLicense = strLicense + "\n    정보처리기사";
+                if (cBAI.isChecked())
+                    strLicense = strLicense + "\n    인공지능데이터전문가";
+                if (cBSecurity.isChecked())
+                    strLicense = strLicense + "\n    정보보안기사";
+
+                it.putExtra("License", strLicense);//이거 또 보내줘야지 catch한 정보
+                setResult(RESULT_OK, it);//제대로 전달, intent를 보내줘야 저쪽에서도 확인하지
+                //setResult(); 보내면 그 순간 결과 전송 저쪽에서 onCreate 발동
+                finish();//더 할 일 없으니 finish
+            }
+        });
+
+
+    }
 }
--- app/src/main/java/com/example/oop_task/MainActivity.java
+++ app/src/main/java/com/example/oop_task/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tVResult, tVResultLabel;
     //class의 Member Field로 추가해
     String str;
+    //editText에서 입력한 ID값을 저장하기 위한 member field
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +26,22 @@ public class MainActivity extends AppCompatActivity {
         EditText editTextId = findViewById(R.id.editTextID);//editText에 있는 글자를 찾아서 저장을 하고 있음
         tVResultLabel = (TextView) findViewById(R.id.tVResultLabel);
         tVResult = (TextView) findViewById(R.id.tVResult);
+        EditText editTextID = findViewById(R.id.editTextID);
 
         //이제 각자 다들 찾았으니 Listener장착해야지
         btnReq.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                str = editTextId.getText().toString();
-                Intent intent = new Intent(getApplicationContext(), InformationActivity.class);
-                intent.putExtra("id", str);
-                startActivityForResult(intent, REQUEST_INFO);
+            public void onClick(View view) {//정보 요청할 때 함수
+                Intent intent = new Intent(getApplicationContext(), InformationActivity.class);//intent 객체 만들어
+                str = editTextID.getText().toString();//id를 꺼내서 계속 가지고 있어 (str에 가지고 있어)
+                //toString을 통해 문자열로 바꿔줘
+
+                intent.putExtra("id", str);// id에 str에 담긴 정보를 넣어줘
+                startActivityForResult(intent, REQUEST_INFO);//결과를 받아와야지
+                // requestCode적어줘, 1이면 처리해도 되는거야
+                // 근데 위에 상수 지정해줘서 REQUEST_INFO라고 적는거지(헷갈릴까봐)
             }
         });
-        btnEnd.setOnClickListener(new View.OnClickListener() {
+        btnEnd.setOnClickListener(new View.OnClickListener() {//btnEnd에도 Listener가 달려 있어야 해
             public void onClick(View view) {
                 finish();
             }
@@ -45,18 +51,24 @@ public class MainActivity extends AppCompatActivity {
     //어떤 결과가 오면 그걸 처리하는 method가 있어야지
     //서브 액티비티[InfomationActivity]가 결과를 보내면 OnActivityResult() 콜백 메소드가 호출
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {//결과를 보내오면 무조건 여기에서 처리돼
+        //결과가 오는 건 싹 다 onActivityResult에서 처리해
+        //requestCode가 맞아야 해! 어디에서 오는 결과인지, 어디로 반환되는지 알아야지
         //@Nullable Intent data 이 매개변수는 intent, 정보가 담겨있어
         super.onActivityResult(requestCode, resultCode, data);
-        if ((requestCode == REQUEST_INFO) && (resultCode == RESULT_OK)) {
-            tVResultLabel.setText("전송\n정보\n출력");
+        if ((requestCode == REQUEST_INFO) && (resultCode == RESULT_OK)) {//requestCode가 맞고 resultCode(상태)가 OK라면
             str = "아이디: " + str;
-            str = str + "\n이름: " + data.getStringExtra("name");
+            str = str + "\n이름: " + data.getStringExtra("name");//putExtra했던 거 꺼내와야지
             str = str + "\n나이: " + data.getStringExtra("age");
             str = str + "\n성별: " + data.getStringExtra("sex");
             str = str + "\n자격증: " + data.getStringExtra("license");
-            tVResult.setText(str);
             //이 위는 인텐트에 실려온 정보를 파악하여 출력
+
+            tVResultLabel.setText("전송\n정보\n출력");
+            tVResult.setText(str);
         }
     }
+
+
+
 }
